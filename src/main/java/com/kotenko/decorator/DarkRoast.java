300ea5aebeee6e58e5cd6dcc834158e7481e1064
Alex <akotenko@prezna.com>
2024-01-31 11:36:23 -0500

    3101L_decorator: added pattern decorator

---
+++ src/main/java/com/kotenko/decorator/DarkRoast.java
@@ -0,0 +1,13 @@
+package com.kotenko.decorator;
+
+public class DarkRoast extends Beverage {
+
+    public DarkRoast() {
+        description = "Dark Roast Coffee";
+    }
+
+    @Override
+    public double cost() {
+        return .99;
+    }
+}
---
+++ src/main/java/com/kotenko/decorator/Decaf.java
@@ -0,0 +1,13 @@
+package com.kotenko.decorator;
+
+public class Decaf extends Beverage {
+
+    public Decaf() {
+        description = "Decaf coffee";
+    }
+
+    @Override
+    public double cost() {
+        return 1.05;
+    }
+}
