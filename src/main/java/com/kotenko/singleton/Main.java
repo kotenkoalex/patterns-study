f1e4cf6fbf21d84953268738dba3a7c1a34c4696
Alex <akotenko@prezna.com>
2024-02-07 23:11:54 -0500

    0702L_sigleton: added pattern singleton

---
+++ src/main/java/com/kotenko/singleton/Main.java
@@ -0,0 +1,9 @@
+package com.kotenko.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Singleton.getInstance());
+        System.out.println(Singleton.getInstance());
+        System.out.println(Singleton.getInstance());
+    }
+}
