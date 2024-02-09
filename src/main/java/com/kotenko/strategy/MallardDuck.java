46c6164749f986c841e6a54ee76d315b3d9876ef
Alex <akotenko@prezna.com>
2024-02-08 22:54:50 -0500

    0802L_strategy: added pattern strategy

---
+++ src/main/java/com/kotenko/strategy/MallardDuck.java
@@ -0,0 +1,14 @@
+package com.kotenko.strategy;
+
+public class MallardDuck extends Duck {
+
+    public MallardDuck() {
+        flyBehavior = new FlyWithWings();
+        quackBehavior = new Quack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm MallardDuck");
+    }
+}
